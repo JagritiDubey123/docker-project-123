52e8031e5a7bd5a68a07c69bd70aa2917c463e9b
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-21 17:27:24 +0530

    Update employeeController.js

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -64,3 +64,19 @@ exports.deleteOne = (req, res)=>{
     else res.status(202).send(data);
   })
 }
+
+
+const Empoloyee = require('../models/Employee')
+const validator = require('validator')
+
+exports.create = (req)=>{
+    if (!req.body) {
+        res.status(400).send({
+          message: "Content can not be empty!"
+        });
+    }
+    // console.log(req.body);
+    const empData = {
+        emp_name : req.body.emp_name,
+        emp_add: req.body.emp_add
+    }
