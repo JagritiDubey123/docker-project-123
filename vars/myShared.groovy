6fc19fecd7f2a8d0057cbf35e7fc33a79e82917e
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-23 15:23:42 +0530

    Update myShared.groovy

--- vars/myShared.groovy
+++ vars/myShared.groovy
@@ -1,7 +1,8 @@
-def call(){
+def call(String name, String dayOfWeek){
   sh 'echo Hi Team, How are you!!!'
   sh "date"
   sh "echo hello "
+  sh " Hello ${name}, Today is ${dayOfWeek"}
  // def call() {
     // Execute npm install command
     sh "sudo apt install npm"
