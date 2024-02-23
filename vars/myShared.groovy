6b171b34412efc6c7746fd2ce997abb83086f723
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-23 11:10:17 +0530

    Update myShared.groovy

--- vars/myShared.groovy
+++ vars/myShared.groovy
@@ -2,48 +2,10 @@ def call(){
   sh 'echo Hi Team, How are you!!!'
   sh "date"
   sh "echo hello "
+ // def call() {
+    // Execute npm install command
+    sh "npm install"
+    sh "npm test"
 }
-// def greet(name) {
-//     sh "echo Hello, $name!"
-// }
-// Define variables and data types
-// def name = "John"
-// def age = 30
-// def fruits = ["apple", "banana", "orange"]
-// def person = [name: "Alice", age: 25]
 
-// // Define a function
-// def greet(name) {
-//     println "Hello, $name!"
-// }
-
-// // Control structures
-// if (age >= 18) {
-//     println "You are an adult."
-// } else {
-//     println "You are a minor."
-// }
-
-// for (fruit in fruits) {
-//     println "I like $fruit."
-// }
-
-// // Define a class
-// class Person {
-//     String name
-//     int age
-
-//     Person(String name, int age) {
-//         this.name = name
-//         this.age = age
-//     }
-
-//     void greet() {
-//         println "Hello, my name is $name and I am $age years old."
-//     }
-// }
-
-// // Create an object of the class
-// // def personObj = new Person("Alice", 25)
-// personObj.greet()
 
